476efcf45cc8a2ba2897f3fc16eb0e7e6dc12d40
its10623 <its10623@gmail.com>
2025-10-15 00:46:35 +0900

    feat: 커스텀 구분자 분리 함수 구현

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -11,4 +11,26 @@ class Calculator {
 
     }
 
+    fun splitCustomDelimiter(input: String): String {
+
+        val customDelimiterStartRange = input.substring(0..1)
+        val customDelimiterEndRange = input.substring(3..4)
+        val customDelimiter = input.substring(2 until 3)
+        val numberPart = input.substring(5)
+
+
+        if (customDelimiterStartRange == CalculatorConstants.CUSTOM_DELIMITER_START) {
+
+            if (customDelimiterEndRange == CalculatorConstants.CUSTOM_DELIMITER_END) {
+
+                CalculatorConstants.CUSTOM_DELIMITER_RESULT = numberPart.split("${customDelimiter}").toString()
+
+            }
+
+        }
+
+        return CalculatorConstants.CUSTOM_DELIMITER_RESULT
+
+    }
+
 }
