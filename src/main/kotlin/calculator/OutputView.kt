03d09d7c581538d74fcb56b9c0c8be1dc955c527
its10623 <its10623@gmail.com>
2025-10-16 14:55:21 +0900

    feat: 출력 문구 OutputView 구현

---
+++ src/main/kotlin/calculator/OutputView.kt
@@ -0,0 +1,17 @@
+package calculator
+
+class OutputView {
+
+    fun printInputMessage(): String{
+
+        return CalculatorConstants.INPUT
+
+    }
+
+    fun printResultMessage(): String {
+
+        return CalculatorConstants.RESULT_OUTPUT
+
+    }
+
+}
