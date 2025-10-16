d30b6c4e6622866f5afc3a722782cbd2696b2910
its10623 <its10623@gmail.com>
2025-10-16 14:55:53 +0900

    feat: 잘못된 입력일 경우 예외 처리 기능 구현

---
+++ src/main/kotlin/calculator/InputValidator.kt
@@ -0,0 +1,44 @@
+package calculator
+
+class InputValidator {
+
+    fun validateDelimiterAndNumber(input: String): String {
+
+        for (char in input) {
+
+            if (validateCustomDelimiterStart(input)) {
+
+                if (char != '/' && char != '\\' && char != 'n' && char.digitToIntOrNull() == null
+                    && char != Calculator().splitCustomDelimiter(input).first()) {
+
+                    throw IllegalArgumentException(CalculatorConstants.ERROR_INVALID_INPUT)
+
+                }
+
+            } else {
+
+                if (char != CalculatorConstants.DELIMITER_FIRST
+                    || char != CalculatorConstants.DELIMITER_SECOND
+                    || char.digitToIntOrNull() == null) {
+
+                    throw IllegalArgumentException(CalculatorConstants.ERROR_INVALID_INPUT)
+
+                }
+            }
+        }
+
+        return input
+
+    }
+
+    fun validateCustomDelimiterStart(input: String): Boolean {
+
+        val validationCustomDelimiterStart = input.startsWith(CalculatorConstants.CUSTOM_DELIMITER_START)
+
+        if (validationCustomDelimiterStart) { return true }
+
+        return false
+
+    }
+
+}
