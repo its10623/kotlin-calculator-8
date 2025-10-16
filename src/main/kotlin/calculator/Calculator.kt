475861cef23228a16b801c616fa27985091c1896
its10623 <its10623@gmail.com>
2025-10-16 16:40:12 +0900

    refactor: 클래스 간결하게 하기 위해 불필요한 함수 제거

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -2,13 +2,12 @@ package calculator
 
 class Calculator {
 
-    fun calculateNumbers (input: String): Int{
+    fun calculateNumbers(input: String): Int {
 
-        val numberList = splitInput(input)
+        val numberStrings = getNumbersAsString(input)
+        var resultNumber = CalculatorConstants.RESULT_NUMBER
 
-        var resultNumber = 0
-
-        for (char in numberList.toString()) {
+        for (char in numberStrings.toString()) {
 
             val digit = char.digitToInt()
 
@@ -20,51 +19,19 @@ class Calculator {
 
     }
 
+    fun getNumbersAsString(input: String): Int {
 
-    fun splitInput(input: String): Int? {
+        val numbersStrings = input.replace(Regex(CalculatorConstants.REPLACE_REGEX_PATTERN), "").toIntOrNull()!!
 
-        var numbersStrings = input.split(CalculatorConstants.DELIMITER_FIRST,
-            CalculatorConstants.DELIMITER_SECOND).toString()
-            .replace(Regex("[^\\d]"),"").toIntOrNull()
-
-        if (hasCustomDelimiter(input)) {
-
-            numbersStrings = specifyCustomDelimiter(input)
-                .replace(Regex("[^\\d]"),"").toIntOrNull()
-
-            return numbersStrings
-
-        } else {
-
-            return numbersStrings
-
-        }
+        return numbersStrings
 
     }
 
     fun splitCustomDelimiter(input: String): String {
 
-        val customDelimiter = input.substring(2 until 3)
+        val customDelimiter = input[CalculatorConstants.CUSTOM_DELIMITER_INDEX].toString()
 
         return customDelimiter
-
-    }
-
-    fun hasCustomDelimiter(input: String): Boolean {
-
-        val customDelimiterBoolean = input.startsWith(CalculatorConstants.CUSTOM_DELIMITER_START)
-        
-        return customDelimiterBoolean
-    }
-
-    fun specifyCustomDelimiter(input: String): String {
-
-        val numberPart = input.substring(5)
-
-        CalculatorConstants.CUSTOM_DELIMITER_NUMBER_RESULT = numberPart
-            .split(splitCustomDelimiter(input)).toString()
-
-        return CalculatorConstants.CUSTOM_DELIMITER_NUMBER_RESULT
 
     }
 
--- src/main/kotlin/calculator/CalculatorConstants.kt
+++ src/main/kotlin/calculator/CalculatorConstants.kt
@@ -5,14 +5,15 @@ object CalculatorConstants {
 
     const val INPUT = "덧셈할 문자열을 입력해 주세요."
     const val RESULT_OUTPUT = "결과 : "
-    const val CUSTOM_DELIMITER_START = "//"
-    const val CUSTOM_DELIMITER_END = """\n"""
-    const val DELIMITER_FIRST = ','
-    const val DELIMITER_SECOND = ':'
-
     const val ERROR_INVALID_INPUT = "잘못된 값이 입력 되었습니다."
 
-    var CUSTOM_DELIMITER_NUMBER_RESULT = ""
-    var BOOLEAN = false
+    const val CUSTOM_DELIMITER_START = "//"
+    const val DELIMITER_FIRST = ','
+    const val DELIMITER_SECOND = ':'
+    const val CUSTOM_DELIMITER_INDEX = 2
+    const val RESULT_NUMBER = 0
+    const val REPLACE_REGEX_PATTERN = "[^\\d]"
+
+
 
 }
